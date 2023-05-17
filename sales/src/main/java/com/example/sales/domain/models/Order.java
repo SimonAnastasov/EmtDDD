9c8d7e73cd6552027ca93e766403697be323f89c
anastasovv <simon@digitalnode.com>
2023-05-17 02:13:42 +0200

    Add dependecy for testing

--- sales/src/main/java/com/example/sales/domain/models/Order.java
+++ sales/src/main/java/com/example/sales/domain/models/Order.java
@@ -6,11 +6,13 @@ import com.example.sales.domain.valueobjects.ShipmentDetails;
 import com.example.sharedkernel.domain.base.AbstractEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
+@Table(name = "shop_order")
 public class Order extends AbstractEntity<OrderId> {
     private ShipmentDetails shipmentDetails;
     private PaymentDetails paymentDetails;
